3ca099455d53edd7ca68eeba813aa08263999033
Martin Tiernan <Martytier75@gmail.com>
2022-12-01 00:51:39 -0600

    Day 1 Complete

---
+++ Code/day1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] lines = System.IO.File.ReadAllLines("E:\\Stuff\\CSprojects\\Advent\\Advent2022\\Data\\day1.txt");
+int elfNum = 0;
+List<List<int>> elfInventory = new();
+elfInventory.Add( new List<int>());
+Console.WriteLine("hello");
+foreach(string line in lines){
+    if(line == ""){
+        elfNum++;
+        elfInventory.Add(new List<int>());
+    }
+    else{
+        elfInventory[elfNum].Add(Int32.Parse(line));
+    }
+}
+List<int> summedList = elfInventory.Select(x => x.Sum()).ToList();
+Console.WriteLine(summedList.Max());
+
+summedList = summedList.OrderByDescending(x => x).ToList();
+Console.WriteLine(summedList[0]);
+int sum = summedList[0] + summedList[1] + summedList[2];
+Console.WriteLine(sum);
---
+++ readme.md
@@ -0,0 +1,2 @@
+<h1>Year 2022: C#</h1>
+Repository for advent of code 2022. 
