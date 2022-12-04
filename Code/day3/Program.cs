765e1b0b10efd35591ec247b8e776266e9ed4d0f
Martin Tiernan <Martytier75@gmail.com>
2022-12-04 10:39:58 -0600

    Day3 and Day4

---
+++ Code/day3/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] lines = System.IO.File.ReadAllLines("E:\\Stuff\\CSprojects\\Advent\\Advent2022\\Data\\day3.txt");
+Dictionary<char,int> values =  new Dictionary<char, int>(){{'A',1},{'B',2}};
+List<string[]> sacks = new();
+foreach(string line in lines){
+    string compartOne = line.Substring(0,(line.Length/2));
+    string compartTwo = line.Substring(line.Length/2, line.Length/2);
+    string[] sack = {compartOne,compartTwo};
+    sacks.Add(sack);
+}
+int sum = 0; 
+
+//part 1
+foreach(string[]sack in sacks){
+    char shared = FindMatching(sack[0],sack[1]);
+    if(char.IsUpper(shared)){
+        sum += shared - 38;
+    }
+    else{
+        sum += shared -96;
+    }
+}
+Console.WriteLine(sum);
+//part 2
+int sum2 =0;
+for(int i = 0; i < lines.Count() -1; i+=3){
+    string one = lines[i];
+    string two = lines[i+1];
+    string three = lines[i+2];
+
+    char shared = FindMatchingThree(one,two,three);
+    if(char.IsUpper(shared)){
+        sum2 += shared - 38;
+    }
+    else{
+        sum2 += shared -96;
+    }
+
+}
+Console.WriteLine(sum2);
+
+char FindMatching(string itemOne,string itemTwo){
+    foreach(char item in itemOne){
+       if(itemTwo.Contains(item)){
+        return item;
+       }
+    }
+    return '0';
+}
+char FindMatchingThree(string one, string two, string three){
+    List<char> matches = new();
+    foreach(char item in one){
+        if(two.Contains(item)){
+            matches.Add(item);
+       }
+    }
+    foreach(char match in matches){
+        if(three.Contains(match)){
+            return match;
+        }
+    }
+    return '0';
+}
---
+++ Code/day4/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] lines = System.IO.File.ReadAllLines("E:\\Stuff\\CSprojects\\Advent\\Advent2022\\Data\\day4.txt");
+
+List<string[]> pairs = new();
+
+foreach(string line in lines){
+    string[] delimiters = {",","-"};
+    string[] split = line.Split(delimiters,StringSplitOptions.None);
+    pairs.Add(split);
+}
+//part 1
+int sum = 0;
+foreach(string[] pair in pairs){
+    if(int.Parse(pair[0]) <= int.Parse(pair[2]) && int.Parse(pair[1]) >= int.Parse(pair[3])){
+        sum++;
+    }
+    else if(int.Parse(pair[2]) <= int.Parse(pair[0]) && int.Parse(pair[3]) >= int.Parse(pair[1])){
+        sum++;
+    }
+}
+Console.WriteLine(sum);
+//part 2
+int sum2 = 0;
+foreach(string[] pair in pairs){
+    if((int.Parse(pair[2]) <= int.Parse(pair[0]) && int.Parse(pair[0]) <= int.Parse(pair[3])) || (int.Parse(pair[2]) <= int.Parse(pair[1]) && int.Parse(pair[1]) <= int.Parse(pair[3])))
+    {
+        sum2++;
+    }
+    else if((int.Parse(pair[0]) <= int.Parse(pair[2]) && int.Parse(pair[2]) <= int.Parse(pair[1])) || (int.Parse(pair[0]) <= int.Parse(pair[3]) && int.Parse(pair[3]) <= int.Parse(pair[1])))
+    {
+        sum2++;
+    }
+}
+Console.WriteLine(sum2);
