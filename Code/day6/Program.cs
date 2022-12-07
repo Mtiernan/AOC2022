f2db03dd6459f505b8406b7a9629e04da5c6255e
Martin Tiernan <Martytier75@gmail.com>
2022-12-06 19:05:22 -0600

    Day 6

---
+++ Code/day6/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] lines = System.IO.File.ReadAllLines("E:\\Stuff\\CSprojects\\Advent\\Advent2022\\Data\\day6.txt");
+Queue<char> queue = new();
+
+int pos = 0;
+bool found =false;
+while(!found){
+    queue.Enqueue(lines[0][pos]);
+    if(queue.Count >= 14){
+        found = queue.Count == String.Join("",queue).Distinct().Count();
+        queue.Dequeue();
+    }
+    pos++;
+}
+Console.WriteLine(pos);
