13c2bd4ff5dea0bd60b0a1a3b84c932d28f06f5d
Martin Tiernan <Martytier75@gmail.com>
2022-12-11 11:03:54 -0600

    day10

---
+++ Code/day10/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] lines = System.IO.File.ReadAllLines("E:\\Stuff\\CSprojects\\Advent\\Advent2022\\Data\\day10.txt");
+
+int cycle = 0; 
+int x = 1;
+int pc = 0;
+int sum = 0;
+int busy =0;
+bool work = true;
+List<int> pixels = new();
+Dictionary<int,int> toAdd = new();
+
+while(work){
+    cycle++;
+    if(x -1 <= (cycle%40)-1 && x + 1 >= (cycle%40)-1){
+        Console.WriteLine($"x:{x} cycle:{cycle%40} {cycle}");
+        pixels.Add(1);
+    }
+    else{
+        pixels.Add(0);
+    }
+    if(busy == 0){
+        switch(lines[pc].Split(' ')[0]){
+            case "noop":
+            break;
+            case "addx":
+                toAdd.Add(cycle+1,int.Parse(lines[pc].Split(' ')[1]));
+                busy = 2;
+            break;
+        }
+         pc++;
+    }
+    if(busy > 0){
+        busy--;
+    }
+
+    if(pc >= lines.Count()){
+       work = false;
+    }
+
+    if(cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220){
+        sum += (cycle * x);
+    }
+    if(toAdd.ContainsKey(cycle)){
+        x += toAdd[cycle];
+        toAdd.Remove(cycle);
+    }
+
+}
+Console.WriteLine(cycle);
+Console.WriteLine(sum);
+
+string[] screen ={"","","","","",""}; 
+for(int i =0; i < pixels.Count(); i++){
+    screen[i/40] += pixels[i] == 1 ? "#" : ".";
+}
+foreach(string line in screen){
+    Console.WriteLine(line);
+}
---
+++ Code/day9/Program.cs
@@ -0,0 +1,153 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] lines = System.IO.File.ReadAllLines("E:\\Stuff\\CSprojects\\Advent\\Advent2022\\Data\\day9.txt");
+
+int[] head = {0,0};
+int[] tail = {0,0};
+
+List<int[]> path = new();
+path.Add(tail);
+//part one
+foreach(string line in lines){
+    switch(line.Split(' ')[0]){
+        case "U":
+            for(int i = 0; i < int.Parse(line.Split(' ')[1]); i++){
+                head[1]--;
+                tail = followHead(head,tail);
+                if(!path.Exists(x => (x[0] == tail[0]  && x[1] == tail[1])))
+                    path.Add(tail);
+            }
+            break;
+        case "D":
+            for(int i = 0; i < int.Parse(line.Split(' ')[1]); i++){
+                head[1]++;
+                tail = followHead(head,tail);
+                if(!path.Exists(x => (x[0] == tail[0]  && x[1] == tail[1])))
+                    path.Add(tail);
+            }
+            break;
+        case "R":
+            for(int i = 0; i < int.Parse(line.Split(' ')[1]); i++){
+                head[0]++;
+                tail = followHead(head,tail);
+                if(!path.Exists(x => (x[0] == tail[0]  && x[1] == tail[1])))
+                    path.Add(tail);
+            }
+            break;
+        case "L":
+         for(int i = 0; i < int.Parse(line.Split(' ')[1]); i++){
+            head[0]--;
+            tail = followHead(head,tail);
+            if(!path.Exists(x => (x[0] == tail[0]  && x[1] == tail[1])))
+                  path.Add(tail);
+        }
+        break;
+    }
+}
+//part 2
+List<int[]> knots = new List<int[]>();
+for(int i =0; i < 10; i++){
+    knots.Add(new int[] {0,0});
+}
+
+List<int[]> path2 = new();
+foreach(string line in lines){
+    switch(line.Split(' ')[0]){
+        case "U":
+            for(int i = 0; i < int.Parse(line.Split(' ')[1]); i++){
+                knots[0][1]--;
+                for(int k=1; k< knots.Count; k++){
+                    knots[k] = followHead(knots[k-1],knots[k]);
+                }
+                if(!path2.Exists(x => (x[0] == knots[9][0]  && x[1] == knots[9][1])))
+                    path2.Add(knots[9]);
+            }
+            break;
+        case "D":
+            for(int i = 0; i < int.Parse(line.Split(' ')[1]); i++){
+                knots[0][1]++;
+                 for(int k=1; k < knots.Count; k++){
+                    knots[k] = followHead(knots[k-1],knots[k]);
+                }
+                if(!path2.Exists(x => (x[0] == knots[9][0]  && x[1] == knots[9][1])))
+                    path2.Add(knots[9]);
+            }
+            break;
+        case "R":
+            for(int i = 0; i < int.Parse(line.Split(' ')[1]); i++){
+                knots[0][0]++;
+                for(int k=1; k < knots.Count; k++){
+                knots[k] = followHead(knots[k-1],knots[k]);
+            }
+                if(!path2.Exists(x => (x[0] == knots[9][0]  && x[1] == knots[9][1])))
+                    path2.Add(knots[9]);
+            }
+            break;
+        case "L":
+         for(int i = 0; i < int.Parse(line.Split(' ')[1]); i++){
+            knots[0][0]--;
+            for(int k=1; k < knots.Count; k++){
+                knots[k] = followHead(knots[k-1],knots[k]);
+            }
+            if(!path2.Exists(x => (x[0] == knots[9][0]  && x[1] == knots[9][1])))
+                  path2.Add(knots[9]);
+        }
+        break;
+    }
+}
+
+int[] followHead(int[]h,int[]t){
+    int[] newTail = {t[0],t[1]};
+    if(Math.Abs(h[0] - t[0]) > 1 || Math.Abs((h[1] - t[1])) > 1){
+        //if x position is the same then we know we need to move up or down
+        if(h[0] == t[0]){
+            //up
+            if(t[1] > h[1]){
+                newTail[1]--;
+            }
+            //down
+            else{
+                newTail[1]++;
+            }
+        }
+        else if(h[1] == t[1]){
+            //left
+            if(t[0] > h[0]){
+                newTail[0]--;
+            }
+            //right
+            else{
+                newTail[0]++;
+            }
+        }
+        else{
+            //up-left
+            if(t[0] > h[0] && t[1] > h[1]){
+                newTail[0]--;
+                newTail[1]--;
+            }
+            //up-right
+            if(t[0] < h[0] && t[1] > h[1]){
+                newTail[0]++;
+                newTail[1]--;
+            }
+            //down-left
+            if(t[0] > h[0] && t[1] < h[1]){
+                newTail[0]--;
+                newTail[1]++;
+            }
+              //down-right
+            if(t[0] < h[0] && t[1] < h[1]){
+                newTail[0]++;
+                newTail[1]++;
+            }
+        }
+    }
+    return newTail;
+}
+
+Console.WriteLine(path.Count);
+Console.WriteLine(path2.Count);
+// foreach(int[] point in path){
+//     Console.WriteLine($"x: {point[0]} y: {point[1]}");
+// }
+
